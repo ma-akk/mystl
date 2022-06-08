cc7025732fafddf73eaa567ff661bd401305b5eb
ma-akk <gtresa@student.21-school.ru>
2022-06-08 19:59:46 +0300

    lexic_compare, equal, some is_integral

--- utils.hpp
+++ utils.hpp
@@ -8,24 +8,51 @@ using std::cout;
 using std::endl;
 
 namespace ft {
-	// template< class InputIt1, class InputIt2 >
-	// bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
-    //                           InputIt2 first2, InputIt2 last2 );
+	template< class InputIt1, class InputIt2 >
+	bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
+                             	 InputIt2 first2, InputIt2 last2 ) {
 
-	// template< class InputIt1, class InputIt2, class Compare >
-	// bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
-    //                           InputIt2 first2, InputIt2 last2,
-    //                           Compare comp );
+		for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 ) {
+			if (*first1 < *first2) return true;
+			if (*first2 < *first1) return false;
+		}
+    	return (first1 == last1) && (first2 != last2);
+	}
 
-	// template< class InputIt1, class InputIt2 >
-	// bool equal( InputIt1 first1, InputIt1 last1,
-	// 		InputIt2 first2 );	
-	// template< class InputIt1, class InputIt2,
-	// 		class BinaryPredicate >
-	// bool equal( InputIt1 first1,
-	// 			InputIt1 last1,
-	// 			InputIt2 first2,
-	// 			BinaryPredicate p );
+	template< class InputIt1, class InputIt2, class Compare >
+	bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
+								InputIt2 first2, InputIt2 last2, Compare comp ) {
+
+		for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 ) {
+			if (comp(*first1, *first2)) return true;
+			if (comp(*first2, *first1)) return false;
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+
+	template< class InputIt1, class InputIt2 >
+	bool equal( InputIt1 first1, InputIt1 last1,
+				InputIt2 first2 ) {
+
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!(*first1 == *first2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template< class InputIt1, class InputIt2, class BinaryPredicate >
+	bool equal( InputIt1 first1, InputIt1 last1,
+				InputIt2 first2, BinaryPredicate p ) {
+
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!p(*first1, *first2)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	template <bool condition, typename T>
 	struct enable_if;
@@ -37,6 +64,38 @@ namespace ft {
 
 	template <typename T>
 	struct is_integral;
+
+	template<class T, bool v>
+	struct integral_constant {
+		static const bool value = v;
+		typedef T value_type;
+		typedef integral_constant type;
+		operator value_type() const { return value; }
+	};
+
+	template <class T> struct is_integral				: public ft::integral_constant<T, false> {};
+	template <> struct is_integral<bool>				: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<char>				: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<signed char>			: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned char>		: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<wchar_t>				: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<char16_t>			: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<short>				: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned short>		: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<int>					: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned int>		: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<long>				: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned long> 		: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<long long>			: public ft::integral_constant<bool, true> {};
+	template <> struct is_integral<unsigned long long>	: public ft::integral_constant<bool, true> {};
+
+	template<bool B, class T, class F>
+		struct conditional {
+		typedef F type;
+	};
+
+	template<class T, class F>
+	struct conditional<true, T, F> { typedef T type; };
 	
 	//testing file is in Standart p.65
 	template <class T1, class T2 >
